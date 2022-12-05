5e570fe3e061d1076b1df710eb22d50253a8348f
Omachi John <omachi.john@yahoo.com>
2022-12-05 00:34:58 -0800

    Create post, add post

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  resources :posts
+  root "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |x|
+     Post.create(title: "Title #{x + 1}", body: "Post number #{x + 1} goes here")
+end
