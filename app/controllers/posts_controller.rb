515ce18165ce9db47e606d246741a1d8128eff57
Omachi John <omachi.john@yahoo.com>
2022-12-06 07:40:23 -0800

    Avoid hitting the database mutiple times when fetching data

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   # GET /posts or /posts.json
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.includes(:user).all.order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
