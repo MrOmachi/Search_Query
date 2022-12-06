0c19b7b97aa06e649d17eae60aea6eb0297c9692
Omachi John <omachi.john@yahoo.com>
2022-12-06 08:19:24 -0800

    Add rails active analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
      before_action :set_query
 
+     before_action :record_page_view
+
+     def record_page_view
+       ActiveAnalytics.record_request(request)
+     end
+
      def set_query 
           @query = Post.ransack(params[:q])
      end
